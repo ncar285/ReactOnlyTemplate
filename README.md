895a23ab5bf3098c4852f3591f73ffdc8334a70d
ncar285 <carlier.nicholas@gmail.com>
2023-12-23 19:28:18 +1300

    add template

---
+++ README.md
@@ -0,0 +1,1 @@
+# fileCabinet-MERN
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './HomePage.css';
+
+const Homepage = () => {
+
+    return (
+        <div>
+            <p>this is a test :)</p>
+            
+        </div>
+    );
+};
+
+export default Homepage;
